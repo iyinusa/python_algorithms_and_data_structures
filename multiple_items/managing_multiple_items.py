ef69bdc197bdb432a30767f8694719d3989a75fd
I. Kennedy Yinusa <iyinusa@yahoo.co.uk>
2024-01-02 17:48:37 +0000

    Update managing_multiple_items.py

--- multiple_items/managing_multiple_items.py
+++ multiple_items/managing_multiple_items.py
@@ -2,11 +2,9 @@
 Creating a program that represents the movements of the line for the lady's bathroom. To begin, create a list that represents the line. It must contain the five names of the women initially waiting (you can make these up).
 
 The following events occur, and you must represent them in the list and print the list out after each action.
-
-    A woman named Jenny arrives who only wanted to check her lipstick, she asks to join the front of the line, and all the women let her.
-    The woman third in line’s phone started ringing, and she left the line to answer.
-    A new woman named Alice joined the line.
-
+    - A woman named Jenny arrives who only wanted to check her lipstick, she asks to join the front of the line, and all the women let her.
+    - The woman third in line’s phone started ringing, and she left the line to answer.
+    - A new woman named Alice joined the line.
 '''
 
 # initial women on the line
